858994dd84481997b337ca540cdf1cb6a726b3f0
Enrico Zappalà <zpp.enrico@gmail.com>
2025-03-06 08:33:00 +0100

    Add test get controller

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
+import { getBooks } from './controllers/books.js';
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/books', getBooks);
 
 export default app;
