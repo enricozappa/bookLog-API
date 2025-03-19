463596fd8aa75e46b9485413ddaa6294cc88f504
Enrico Zappalà <zpp.enrico@gmail.com>
2025-03-19 18:56:00 +0100

    Add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express, { Application } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import { getBooks, addBook, updateBook, deleteBook } from './controllers/book.controller.js';
 
 const app: Application = express();
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE']
+}));
 
 app.get('/api/books', getBooks);
 app.post('/api/books', addBook);
